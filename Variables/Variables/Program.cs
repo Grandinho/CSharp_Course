913fc4bafc16a86a7c10beda659466baa77f61b2
Grandinho <monyx5861@gmail.com>
2022-06-25 11:00:33 +0200

    Added variables project

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaring a variable
+            int num1;
+
+            // assigning a value to a variable
+            num1 = 13;
+
+            //declaring and initializing a variable in one go
+            int num2 = 23;
+       
+            int sum = num1 + num2;
+            Console.WriteLine(sum);
+        }
+    }
+}
