24408db7259742a096bee2b0384acd7fea30946c
Grandinho <monyx5861@gmail.com>
2022-07-30 17:56:04 +0200

    Added ParsingVariables

---
+++ Variables/ParsingVariables/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ParsingVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parsing
+            string myString = "15";
+            string mySecondString = "13";
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+
+            int result1 = num1 + num2;
+
+            Console.WriteLine(result1);
+            Console.Read();
+
+            //Exercise 1
+
+            string stringForFloat = "0.85"; // datatype should be float
+            string stringForInt = "12345"; // datatype should be int
+
+            float numForFloat = float.Parse(stringForFloat);
+            int numForInt = int.Parse(stringForInt);
+
+            
+        }
+    }
+}
