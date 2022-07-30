95fd0030855d7138d83cf2dfe47edb32e1f2f736
Grandinho <monyx5861@gmail.com>
2022-07-30 17:57:02 +0200

    Complited first exercise: Datatypes

---
+++ Variables/Challenge - Datatypes And Variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge___Datatypes_And_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte myByte = 255;
+            sbyte mySbyte = 127;
+            int myInt = -3213213;
+            int myUInt = 231321;
+            float myFlaot = 0.432f;
+            double myDouble = 23.32;
+            char myChar = 'A';
+            bool myBool = true;
+
+            string myText = "I control text";
+            string numText = "15";
+
+            int myNumFromText = int.Parse(numText);
+            Console.WriteLine(myNumFromText);
+        }
+    }
+}
