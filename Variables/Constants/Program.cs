1cc5e30ec0375f3e84a6facff988c0be0f2d7fbd
Grandinho <monyx5861@gmail.com>
2022-07-30 21:15:50 +0200

    Update

---
+++ Variables/Constants/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    //Constants are immutable values which are known
+    // at compile time and do not change for the life of the program.
+
+    class Program
+    {
+        const double PI = 3.14159265;
+        const int weeks = 52, months = 12;
+        const int days = 365;
+        const string birthday = "01.02.2004";
+        static void Main(string[] args)
+        {
+            // days = 366; //unpossible because its const 
+            Console.WriteLine("My birthday is always going to be: {0}", birthday);
+            Console.ReadLine();
+        }
+    }
+}
