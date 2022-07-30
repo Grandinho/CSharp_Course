3d2c14a3adcd53fa79abc0f1ee36672dfee19a88
Grandinho <monyx5861@gmail.com>
2022-07-30 17:55:43 +0200

    Added string methods

---
+++ Variables/StringMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName = "Denis";
+            string lastName = "Panjuta";
+            string fullName = string.Concat(" ", firstName, lastName, " ");
+            Console.WriteLine(firstName.Substring(2)); //output: "nis"
+            Console.WriteLine(firstName.ToLower()); //output: "denis"
+            Console.WriteLine(firstName.ToUpper()); //output: "DENIS"
+            Console.WriteLine(fullName.Trim()); //output: "DenisPanjuta" 
+            Console.WriteLine(firstName.IndexOf('e')); //output: 1
+            Console.WriteLine(String.IsNullOrWhiteSpace(firstName)); //output: false
+            Console.WriteLine(fullName);
+
+            Console.ReadKey();
+        }
+    }
+}
