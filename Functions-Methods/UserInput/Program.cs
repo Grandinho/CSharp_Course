84fcbf13b25d67fed4d420bd0458d7e1e58ee3ff
Grandinho <monyx5861@gmail.com>
2022-07-31 12:35:35 +0200

    Added UserInput

---
+++ Functions-Methods/UserInput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, Add(num1, num2));
+            Console.ReadLine();
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
