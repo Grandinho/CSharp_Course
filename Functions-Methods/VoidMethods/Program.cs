091abfc79719bb1f82cadd24e9a27c6c19610f9e
Grandinho <monyx5861@gmail.com>
2022-07-31 11:35:58 +0200

    Added void methods

---
+++ Functions-Methods/VoidMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteSomething();
+            WriteSomethingSpecific("Some message");
+            Console.ReadLine();
+        }
+
+        //Access modifier (static) return type method name(params)
+        //Void method without parameters
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+
+        }
+
+        //Access modifier (static) return type method name(params)
+        //Voide method with parameters
+        public static void WriteSomethingSpecific(string _message)
+        {
+            Console.WriteLine(_message);
+        }
+    }
+}
